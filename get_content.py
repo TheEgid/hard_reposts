f1c9a339780507da2591862305fe386ad389e2f9
TheEgid <40724661+TheEgid@users.noreply.github.com>
2019-02-19 10:13:58 +0300

    Create get_content.py

---
+++ get_content.py
@@ -0,0 +1,22 @@
+import logging
+import os
+
+def get_content_from_files_system(content_number)
+    dir_name = 'content_folder'
+    text_extension = '.jpg'
+    img_extension = '.txt'
+    if not os.path.exists(dir_name):
+        os.makedirs(dir_name)
+    content_text_file_path_and_name = dir_name+'/'+str(content)+'/'+text_extension
+    content_img_file_path_and_name = dir_name+'/'+str(content)+'/'+img_extension	
+    if not os.path.isfile(content_text_file_path_and_name): 
+        except FileNotFoundError:
+			logging.exception('content_text file error! check the content_folder')
+			raise FileNotFoundError('content_text error!')
+    if not os.path.isfile(content_img_file_path_and_name):
+        except FileNotFoundError:
+			logging.exception('content_img file error! check the content_folder')
+			raise FileNotFoundError('content_img error!')
+    with open(content_text_file_path_and_name, 'r') as inp_file:
+    	content_text = inp_file.read()
+    return (content_text, content_img_file_path_and_name)
