8756ebbc6483cf49a05adaece6f889e71e4cdd3d
TheEgid <40724661+TheEgid@users.noreply.github.com>
2019-02-25 20:35:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -76,9 +76,6 @@ def get_args_parser():
 
 
 def post_all(content_number):
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    sys.path.insert(0, os.path.split(dir_path)[0])
-    logging.basicConfig(level=logging.INFO)
     load_dotenv()
 
     LOGIN_VK = os.getenv("LOGIN_VK")
@@ -123,6 +120,7 @@ def post_all(content_number):
 if __name__ == '__main__':
     dir_path = os.path.dirname(os.path.realpath(__file__))
     sys.path.insert(0, os.path.split(dir_path)[0])
+    logging.basicConfig(level=logging.INFO)
     arg_parser = get_args_parser()
     args = arg_parser.parse_args()
     post_all(args.number)
